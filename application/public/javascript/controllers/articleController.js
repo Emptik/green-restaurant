0f8323a175a2cdd4b2b81ad8a9eccee181491ab8
Emptik <kevin.martin59650@gmail.com>
2021-09-20 18:34:06 +0200

    Remove prefix component and add nested selector

--- application/public/javascript/controllers/articleController.js
+++ application/public/javascript/controllers/articleController.js
@@ -3,8 +3,8 @@ import Article from '../views/ArticleView.js';
 const articleController = () => {
     const articleContentLeft = document.querySelector('.article__content--leftTargeted');
     const articleContentRight = document.querySelector('.article__content--rightTargeted');
-    const btnInfoLeft = document.querySelector('.c-btn--infoLeftClicked');
-    const btnInfoRight = document.querySelector('.c-btn--infoRightClicked');
+    const btnInfoLeft = document.querySelector('.btn--infoLeftClicked');
+    const btnInfoRight = document.querySelector('.btn--infoRightClicked');
     const articleContentVisibility = 'article__content--visible';
 
     const article = new Article(
