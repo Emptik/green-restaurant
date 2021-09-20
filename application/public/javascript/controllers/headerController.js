68b8d7686f49e89eab9344b756d517f917856ae6
Emptik <kevin.martin59650@gmail.com>
2021-09-20 18:54:12 +0200

    Remove prefix component and add nested selector

--- application/public/javascript/controllers/headerController.js
+++ application/public/javascript/controllers/headerController.js
@@ -2,11 +2,11 @@ import Header from '../views/HeaderView.js';
 
 const headerController = () => {
     const toggleElement = document.querySelector('.c-toggle--clicked');
-    const headerElement = document.querySelector('.c-header--targeted');
-    const headerNavBarElement = document.querySelector('.c-header__navbar--targeted');
-    const headerClose = 'c-header--close';
-    const headerOpen = 'c-header--open';
-    const headerNavBarVisibility = 'c-header__navbar--visible';
+    const headerElement = document.querySelector('.header--targeted');
+    const headerNavBarElement = document.querySelector('.header__navbar--targeted');
+    const headerClose = 'header--close';
+    const headerOpen = 'header--open';
+    const headerNavBarVisibility = 'header__navbar--visible';
 
     const header = new Header(
         toggleElement,
