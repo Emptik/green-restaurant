80059485806b47f660ea520ef0fd231c79e5290b
Emptik <kevin.martin59650@gmail.com>
2021-09-20 18:20:52 +0200

    Remove prefix component

--- application/public/javascript/controllers/articleController.js
+++ application/public/javascript/controllers/articleController.js
@@ -1,11 +1,11 @@
 import Article from '../views/ArticleView.js';
 
 const articleController = () => {
-    const articleContentLeft = document.querySelector('.c-article__content--leftTargeted');
-    const articleContentRight = document.querySelector('.c-article__content--rightTargeted');
+    const articleContentLeft = document.querySelector('.article__content--leftTargeted');
+    const articleContentRight = document.querySelector('.article__content--rightTargeted');
     const btnInfoLeft = document.querySelector('.c-btn--infoLeftClicked');
     const btnInfoRight = document.querySelector('.c-btn--infoRightClicked');
-    const articleContentVisibility = 'c-article__content--visible';
+    const articleContentVisibility = 'article__content--visible';
 
     const article = new Article(
         articleContentLeft,
