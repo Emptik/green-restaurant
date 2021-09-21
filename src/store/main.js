8c1306f42e2508a1147a5789e6a48980c7ff663a
Emptik <kevin.martin59650@gmail.com>
2021-09-21 23:03:44 +0200

    Create store

---
+++ src/store/main.js
@@ -0,0 +1,9 @@
+import button from './modules/Button.js';
+
+const store = {
+    modules: {
+        button,
+    },
+};
+
+export default store;
---
+++ src/store/modules/Button.js
@@ -0,0 +1,11 @@
+class Button {
+    constructor() {
+        this.buttonTarget = null;
+    }
+
+    setButtonTarget(buttonTarget) {
+        this.buttonTarget = buttonTarget;
+    }
+}
+
+export default new Button();
